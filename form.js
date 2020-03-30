2de633c5026f2f21515272d2cc8e945a12dc29ad
yennle <56371037+yennle@users.noreply.github.com>
2020-03-29 21:31:42 -0400

    fix Symptom button

--- form.js
+++ form.js
@@ -78,27 +78,32 @@
 
 // Ask how long traveler have been in China 
 var inChina = document.getElementById("inChina");
-inChina.addEventListener("change", function (e) {
+if(inChina){
+  inChina.addEventListener("click", function (e) {
+    if (this.checked) {
+      document.getElementById("dateChina").style.display = "block";
+    }
+    else {
+      document.getElementById("dateChina").style.display = "none";
+    }
+  });
+}
 
-  if (this.checked) {
-    document.getElementById("dateChina").style.display = "block"
-  }
-  else {
-    document.getElementById("dateChina").style.display = "none"
-  }
-});
 
 // Ask how long traveler have been in countries/regions
 var inRegion = document.getElementById("inRegion");
-inRegion.addEventListener("change", function (e) {
+if(inRegion){
+  inRegion.addEventListener("click", function (e) {
 
-  if (this.checked) {
-    document.getElementById("dateRegion").style.display = "block"
-  }
-  else {
-    document.getElementById("dateRegion").style.display = "none"
-  }
-});
+    if (this.checked) {
+      document.getElementById("dateRegion").style.display = "block"
+    }
+    else {
+      document.getElementById("dateRegion").style.display = "none"
+    }
+  });
+}
+
 
 ///////////////////////////////////////////////
 // Flight Information                        //
@@ -234,27 +239,105 @@ function onKeyDown(e) {
 
 //Number of stops
 var stop_element = document.getElementById("stop_number")
-stop_element.addEventListener("change", function (e) {
-  var stop_num = stop_element.options[stop_element.selectedIndex].value;
-  var many_stops= this.parentNode.getElementsByClassName("many_stops")
-  if (stop_num == 0) {
-    document.getElementById("no_stops").style.display = "block"
-    for(var i=0; i<many_stops.length;i++)
-    {
-      many_stops[i].style.display="none"
+if(stop_element){
+  stop_element.addEventListener("change", function (e) {
+    var stop_num = stop_element.options[stop_element.selectedIndex].value;
+    var many_stops= this.parentNode.getElementsByClassName("many_stops")
+    if (stop_num == 0) {
+      document.getElementById("no_stops").style.display = "block"
+      for(var i=0; i<many_stops.length;i++)
+      {
+        many_stops[i].style.display="none"
+      }
+      //document.getElementsByClassName("many_stops").style.display = "none"
+      //alert(stop_num)
     }
-    //document.getElementsByClassName("many_stops").style.display = "none"
-    //alert(stop_num)
-  }
-  else {
-    document.getElementById("no_stops").style.display = "none"
-
-    for (var i = 0; i <stop_num; i++) {
-      many_stops[i].style.display="block"
+    else {
+      document.getElementById("no_stops").style.display = "none"
+  
+      for (var i = 0; i <stop_num; i++) {
+        many_stops[i].style.display="block"
+      }
     }
-  }
-});
+  });
+}
 
+///////////////////////////////////////////////
+// Symptoms                                  //
+///////////////////////////////////////////////
+
+// Get all symtoms elements
+var isFever= document.getElementById("isFever");
+var notFever= document.getElementById("notFever");
+var isCough= document.getElementById("isCough");
+var notCough= document.getElementById("notCough");
+var isShortBreath= document.getElementById("isShortBreath");
+var notShortBreath= document.getElementById("notShortBreath");
+
+// Fever = True
+if(isFever){
+  isFever.addEventListener("click",function(){
+    if(notFever.classList.contains("false")){
+      notFever.classList.remove("false")
+    }
+    isFever.classList.add("true")
+    //alert(isFever.className)
+  });
+}
+
+// Fever = False
+if(notFever){
+  notFever.addEventListener("click",function(){
+    if(isFever.classList.contains("true")){
+      isFever.classList.remove("true")
+    }
+    notFever.classList.add("false")
+    //alert(notFever.className)
+  });
+}
+
+
+// Cough = True
+if(isCough){
+  isCough.addEventListener("click",function(){
+    if(notCough.classList.contains("false")){
+      notCough.classList.remove("false")
+    }
+    isCough.classList.add("true")
+   // alert(isCough.className)
+  });
+}
+
+// Cough = False
+
+if(notCough){
+  notCough.addEventListener("click",function(){
+    if(isCough.classList.contains("true")){
+      isCough.classList.remove("true")
+    }
+    notCough.classList.add("false")
+    //alert(notCough.className)
+  });
+}
+
+// Short Breath = True
+
+if(isShortBreath){
+  isShortBreath.addEventListener("click",function(){
+    
+    isShortBreath.classList.add("true")
+    //alert(isShortBreath.className)
+  });
+}
+
+// Short Breath = False
+
+if(notShortBreath){
+  notShortBreath.addEventListener("click",function(){
+    notShortBreath.classList.add("false")
+    //alert(notShortBreath.className)
+  });
+}
 
 
 
