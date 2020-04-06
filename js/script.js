90824594338901d8e2f31db086915bf2c1404a45
nicomwang <nicomwang@gmail.com>
2020-04-06 11:34:13 -0400

    Fix search refresh issue

--- js/script.js
+++ js/script.js
@@ -1,36 +1,21 @@
-
 ///////////////////////////////////////////////
 // Tab                                      //
 /////////////////////////////////////////////
 function showTableContent(evt, Name) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(Name).style.display = "block";
-    evt.currentTarget.className += " active";
-    //alert("graph: "+document.getElementById("graphs").style.display)
-  //alert("passenger: "+document.getElementById("passenger").style.display)
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  
-  // Get the element with id="defaultOpen" and click on it
- //document.getElementById("defaultOpen").click();
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Name).style.display = "block";
+  evt.currentTarget.className += " active";
+  //alert("graph: "+document.getElementById("graphs").style.display)
+  //alert("passenger: "+document.getElementById("passenger").style.display)
+}
 
-// Search
-var search_form= document.getElementById("search-form");
-search_form.addEventListener("submit",function(){
-  
-   document.getElementById("graphs").style.display="none"
-   document.getElementById("passenger").style.display="block"
-  //   alert("graph: "+document.getElementById("graphs").style.display)
-  //   alert("passenger: "+document.getElementById("passenger").style.display)
-});
-
-///////////////////////////////////////////////
-// Chart                                   //
-/////////////////////////////////////////////
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
--- view_data.php
+++ view_data.php
@@ -1,133 +1,21 @@
 <?php
 $connect = mysqli_connect("localhost", "root", "wit123", "covid_declaration");
 $error = mysqli_connect_error();
+
 //Display error if cannot connect to database
 if ($error != null) {
     $output = "<p>Uable to connect to database</p>" . $error;
     exit($output);
 }
+
 ?>
 
 <html>
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style2.css">
-</head>
-
-<body>
-    <div class="main">
-    <button id="logout-btn"><a href="http://localhost/COVID-Declaration/login.html">Logout</a></button>
-        <img src="images/tsabanner.png" alt="TSA logo" /></a><br /><br />
-        <div class="container">
-
-            <div class="tab">
-                <button id= "overview-tab" class="tablinks" onclick="showTableContent(event, 'graphs')" >Overview</button>
-                <button id= "passenger-tab"  class="tablinks" onclick="showTableContent(event, 'passenger')">Passenger</button>
-            </div>
-            <!-- Overview: Graphs -->
-            <div id="graphs" class="tabcontent">
-                <h2>Health Decalaration Overview</h2>
-                <table class="columns">
-                    <tr>
-                        <td>
-                            <div id="piechart_div" style="border: 1px solid #ccc"></div>
-                        </td>
-                        <td>
-                            <div id="barchart_div" style="border: 1px solid #ccc"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-
-            <!-- Passenger: search field -->
-            <div id="passenger" class="tabcontent">
-                <h2>Search Passenger </h2>
-                <form id="search-form" method="POST">
-                    <fieldset>
-                        <p>Please enter passenger's last name or passport number</p>
-                        <input type="text" name="search" placeholder="Enter search string..." />
-                        <input type="submit" name="submit" value="Search" />
-                    </fieldset>
-                </form><br />
-                <!-- Passenger: Table -->
-                <div>
-                    <div id="table_div">
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-    <script src="js/script.js"></script>
-    
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <!-- Table -->
-    <script type="text/javascript">
-                        google.charts.load('current', {
-                            'packages': ['table']
-                        });
-                        google.charts.setOnLoadCallback(drawTable);
-                        
-                        function drawTable() {
-                            var data = new google.visualization.DataTable();
-                            data.addColumn('string', 'First Name');
-                            data.addColumn('string', 'Last Name');
-                            data.addColumn('string', 'Passport Number');
-                            data.addColumn('string', 'Nationality');
-                            data.addColumn('boolean', 'Visited China');
-                            data.addColumn('boolean', 'Visited Restricted Regions');
-                            data.addColumn('boolean', 'Cough');
-                            data.addColumn('boolean', 'Fever');
-                            data.addColumn('boolean', 'Difficulty Breathing');
-                            data.addColumn('string', 'Email');
-
-                            data.addRows([
-                                <?php
-                                if (isset($_POST['search'])) {
-                                    $searchQuery = $_POST['search'];
-                                    $sql = "Select 
-                                                    firstName,
-                                                    lastName,
-                                                    passportNo, 
-                                                    nationality,
-                                                    if(inChina,'true','false')inChina,
-                                                    if(inRegions,'true','false')inRegions, 
-                                                    if(cough,'true','false') cough,
-                                                    if(fever,'true','false')fever, 
-                                                    if(difficultyBreathing,'true','false')difficultyBreathing,
-                                                    email
-                                                from 
-                                                    passenger 
-                                                WHERE passportNo LIKE '%$searchQuery%'OR LastName LIKE '%$searchQuery%'";
-                                    $result = mysqli_query($connect, $sql);
-                                    if ($result->num_rows > 0) {
-                                        while ($row = mysqli_fetch_array($result)) {
-                                            echo "[
-                                                 '" . $row["firstName"] . "','" . $row["lastName"] . "','" . $row["passportNo"] . "','" . $row["nationality"] . "',
-                                                 " . $row["inChina"] . "," . $row["inRegions"] . ", " . $row["cough"] . "," . $row["fever"] . "," . $row["difficultyBreathing"] . ",
-                                                 '" . $row["email"] . "'
-                                                ], ";
-                                        }
-                                    } else {
-                                        echo "No search results!";
-                                    }
-                                }
-                                ?>
-                            ]);
-
-                            var table = new google.visualization.Table(document.getElementById('table_div'));
-                            //alert("graph: "+document.getElementById("graphs").style.display)
-                            //alert("passenger: "+document.getElementById("passenger").style.display)
-                            table.draw(data, {
-                                showRowNumber: true,
-                                width: '100%',
-                                height: '500'
-                            });
-                            
-                        }
-                        
-                    </script>   
-    <!-- Chart -->
     <script type="text/javascript">
         // Load Charts and the corechart and barchart packages.
         google.charts.load('current', {
@@ -192,7 +80,119 @@ if ($error != null) {
             barchart.draw(data, barchart_options);
         }
     </script>
-    
+</head>
+
+<body>
+    <button id="logout-btn"><a href="http://localhost/COVID-Declaration/login.html">Logout</a></button>
+    <div class="main">
+        <img src="images/tsabanner.png" alt="TSA logo" /></a><br /><br />
+        <div class="container">
+
+            <div class="tab">
+                <button class="active" onclick="showTableContent(event, 'passenger')" id="defaultOpen">Passenger</button>
+                <button class="tablinks" onclick="showTableContent(event, 'graphs')">Distrubution</button>
+
+            </div>
+
+            <!-- Passenger Tab -->
+            <div id="passenger" class="tabcontent">
+                <h2>Search Passenger </h2>
+                <form method="POST">
+                    <fieldset>
+                        <p>Please enter passenger's last name or passport number</p>
+                        <input type="text" name="search" placeholder="Enter search string..." />
+                        <input type="submit" name="submit" value="Search" />
+                    </fieldset>
+                </form><br />
+
+                <!-- Passenger content: Table -->
+                <div>
+                    <script type="text/javascript">
+                        google.charts.load('current', {
+                            'packages': ['table']
+                        });
+                        google.charts.setOnLoadCallback(drawTable);
+
+                        function drawTable() {
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'First Name');
+                            data.addColumn('string', 'Last Name');
+                            data.addColumn('string', 'Passport Number');
+                            data.addColumn('string', 'Nationality');
+                            data.addColumn('boolean', 'Visited China');
+                            data.addColumn('boolean', 'Visited Restricted Regions');
+                            data.addColumn('boolean', 'Cough');
+                            data.addColumn('boolean', 'Fever');
+                            data.addColumn('boolean', 'Difficulty Breathing');
+                            data.addColumn('string', 'Email');
+
+                            data.addRows([
+                                <?php
+                                if (isset($_POST['search'])) {
+                                    $searchQuery = $_POST['search'];
+                                    $sql = "Select 
+                                                    firstName,
+                                                    lastName,
+                                                    passportNo, 
+                                                    nationality,
+                                                    if(inChina,'true','false')inChina,
+                                                    if(inRegions,'true','false')inRegions, 
+                                                    if(cough,'true','false') cough,
+                                                    if(fever,'true','false')fever, 
+                                                    if(difficultyBreathing,'true','false')difficultyBreathing,
+                                                    email
+                                                from 
+                                                    passenger 
+                                                WHERE passportNo LIKE '%$searchQuery%'OR LastName LIKE '%$searchQuery%'";
+                                    $result = mysqli_query($connect, $sql);
+                                    if ($result->num_rows > 0) {
+                                        while ($row = mysqli_fetch_array($result)) {
+                                            echo "[
+                                                 '" . $row["firstName"] . "','" . $row["lastName"] . "','" . $row["passportNo"] . "','" . $row["nationality"] . "',
+                                                 " . $row["inChina"] . "," . $row["inRegions"] . ", " . $row["cough"] . "," . $row["fever"] . "," . $row["difficultyBreathing"] . ",
+                                                 '" . $row["email"] . "'
+                                                ], ";
+                                        }
+                                    } else {
+                                        echo "No search results!";
+                                    }
+                                }
+                                ?>
+                            ]);
+
+                            var table = new google.visualization.Table(document.getElementById('table_div'));
+
+                            table.draw(data, {
+                                showRowNumber: true,
+                                width: '100%',
+                                height: '400'
+                            });
+                        }
+                    </script>
+                    <div id="table_div"></div>
+                </div>
+
+            </div>
+
+            <!-- Distrubution Tab -->
+            <div id="graphs" class="tabcontent">
+                <h2>Health Decalaration Overview</h2>
+                <table class="columns">
+                    <tr>
+                        <td>
+                            <div id="piechart_div" style="border: 1px solid #ccc"></div>
+                        </td>
+                        <td>
+                            <div id="barchart_div" style="border: 1px solid #ccc"></div>
+                        </td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
 </body>
 
 </html>
