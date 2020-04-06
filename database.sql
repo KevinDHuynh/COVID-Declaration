01532540705afd09149fd0c7b143cb2beaf6b2bb
Kevin Huynh <kevinhuynh9464@gmail.com>
2020-04-06 19:28:17 -0400

    Update database.sql
    add LayoverID

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 11:57 PM
+-- Generation Time: Apr 07, 2020 at 01:28 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `covid_declaration`
 --
-CREATE DATABASE IF NOT EXISTS `covid_declaration` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `covid_declaration`;
 
 -- --------------------------------------------------------
 
@@ -30,7 +28,6 @@ USE `covid_declaration`;
 -- Table structure for table `flight`
 --
 
-DROP TABLE IF EXISTS `flight`;
 CREATE TABLE `flight` (
   `flightID` int(32) NOT NULL,
   `origin` varchar(3) NOT NULL,
@@ -53,7 +50,19 @@ INSERT INTO `flight` (`flightID`, `origin`, `destination`, `stopNo`, `passengerI
 (66666, 'PHX', 'BOS', 0, 10),
 (77777, 'MIA', 'BOS', 0, 8),
 (88888, 'EWR', 'SFO', 1, 7),
-(98089, 'DFW', 'BOS', 0, 5);
+(98089, 'DFW', 'BOS', 0, 5),
+(98095, 'qwe', 'qwe', 0, 91346),
+(98096, 'qwe', 'qwe', 1, 91347),
+(98097, 'qwe', 'qwe', 3, 91348),
+(98098, 'qwe', 'qwe', 3, 91349),
+(98099, 'qwe', 'qwe', 3, 91350),
+(98100, 'qwe', 'qwe', 3, 91351),
+(98101, 'qwe', 'qwe', 3, 91352),
+(98102, 'qwe', 'qwe', 3, 91353),
+(98103, 'qwe', 'qwe', 3, 91354),
+(98104, 'qwe', 'qwe', 3, 91355),
+(98105, 'qwe', 'qwe', 3, 91356),
+(98106, 'qwe', 'qwe', 3, 91357);
 
 -- --------------------------------------------------------
 
@@ -61,8 +70,8 @@ INSERT INTO `flight` (`flightID`, `origin`, `destination`, `stopNo`, `passengerI
 -- Table structure for table `layover`
 --
 
-DROP TABLE IF EXISTS `layover`;
 CREATE TABLE `layover` (
+  `layoverID` int(11) NOT NULL,
   `flightID` int(32) NOT NULL,
   `stopNo` int(11) NOT NULL,
   `origin` varchar(3) NOT NULL,
@@ -75,11 +84,11 @@ CREATE TABLE `layover` (
 -- Dumping data for table `layover`
 --
 
-INSERT INTO `layover` (`flightID`, `stopNo`, `origin`, `destination`, `flightNo`, `seatNo`) VALUES
-(21331, 0, 'BOS', 'JFK', 111111111, '27A'),
-(44444, 0, 'LAX', 'MIA', 34352, '01B'),
-(44444, 0, 'MIA', 'DOV', 432543, '10D'),
-(55555, 1, 'LAX', 'JFK', 22323, '12C');
+INSERT INTO `layover` (`layoverID`, `flightID`, `stopNo`, `origin`, `destination`, `flightNo`, `seatNo`) VALUES
+(6, 21331, 0, 'BOS', 'JFK', 111111111, '27A'),
+(7, 44444, 0, 'LAX', 'MIA', 34352, '01B'),
+(8, 44444, 0, 'MIA', 'DOV', 432543, '10D'),
+(9, 55555, 1, 'LAX', 'JFK', 22323, '12C');
 
 -- --------------------------------------------------------
 
@@ -87,7 +96,6 @@ INSERT INTO `layover` (`flightID`, `stopNo`, `origin`, `destination`, `flightNo`
 -- Table structure for table `passenger`
 --
 
-DROP TABLE IF EXISTS `passenger`;
 CREATE TABLE `passenger` (
   `passengerID` int(32) NOT NULL,
   `passportNo` varchar(32) NOT NULL,
@@ -103,7 +111,6 @@ CREATE TABLE `passenger` (
   `zipcode` int(5) DEFAULT NULL,
   `email` varchar(32) NOT NULL,
   `phone` bigint(10) DEFAULT NULL,
-  `mobile` tinyint(1) NOT NULL,
   `inChina` tinyint(1) NOT NULL,
   `date_inChina` date DEFAULT NULL,
   `fever` tinyint(1) NOT NULL,
@@ -117,17 +124,32 @@ CREATE TABLE `passenger` (
 -- Dumping data for table `passenger`
 --
 
-INSERT INTO `passenger` (`passengerID`, `passportNo`, `nationality`, `firstName`, `middleName`, `lastName`, `DOB`, `gender`, `street`, `city`, `state`, `zipcode`, `email`, `phone`, `mobile`, `inChina`, `date_inChina`, `fever`, `cough`, `difficultyBreathing`, `inRegions`, `recentCountries`) VALUES
-(1, '111111111', 'Chinese', 'Nico', NULL, 'Wang', NULL, 'female', NULL, NULL, NULL, NULL, 'nicowang@gmail.com', 1112223333, 0, 1, '2020-03-02', 0, 0, 0, 0, NULL),
-(2, 'K12342352', 'British', 'Joellyn', NULL, 'Panton', '1954-01-08', 'Female', '7690 Florence Parkway', 'Nashville', 'TN', 37245, 'jpanton0@discovery.com', 6151307355, 1, 0, NULL, 0, 0, 0, 0, NULL),
-(3, 'G35424432', 'American', 'Rutherford', NULL, 'Wisker', '1975-04-05', 'Male', '9333 Tennessee Circle', 'Jacksonville', 'FL', 32220, 'rwisker1@acquirethisname.com', 9047951236, 0, 0, NULL, 0, 0, 0, 0, NULL),
-(4, 'G223187', 'Italian', 'Lucia', 'B', 'Rossi', '1992-02-11', 'female', '100 Boylston st', 'Boston', 'MA', 20116, 'rossil@gmail.com', 376383231, 1, 0, NULL, 0, 0, 0, 0, NULL),
-(5, '015042343', 'American', 'Alfonso', NULL, 'Velez', '1930-12-02', 'Male', '4 Ruskin Street', 'Charlotte', 'NC', 28220, 'avelez2@soundcloud.com', 7047616229, 0, 1, '2020-03-23', 1, 0, 1, 0, NULL),
-(6, 'X02430033', 'American', 'Jase', NULL, 'Halsall', '1971-08-17', 'Male', '5557 Thackeray Crossing', 'Gainesville', 'GA', 30506, 'jhalsall3@xinhuanet.com', 7702996126, 1, 0, NULL, 1, 0, 1, 1, 'Italy'),
-(7, '233336662', 'American', 'Misty', 'K', 'Dangl', '1960-10-14', 'Female', '27 Ohio Avenue', 'Des Moines', 'NY', 50936, 'mdangl4@netlog.com', 5151137228, 0, 1, '2020-01-01', 0, 1, 0, 0, NULL),
-(8, '673232412', 'American', 'Towney', NULL, 'Huyghe', '1937-04-25', 'Male', '40 Vermont Junction', 'Houston', 'TX', 77020, 'thuyghe5@shop-pro.jp', 7136167287, 0, 1, '2020-01-24', 0, 1, 1, 1, 'Finland'),
-(9, '687935325', 'American', 'Benji', NULL, 'Boman', '1930-09-03', 'Male', '2785 Drewry Avenue', 'El Paso', 'TX', 79916, 'bboman6@twitpic.com', 9151320033, 0, 1, '2020-02-29', 0, 0, 0, 0, NULL),
-(10, '337352266', 'American', 'Sidonnie', NULL, 'Abramof', '1933-08-13', 'Female', '5805 Talmadge Alley', 'El Paso', 'MA', 79950, 'sabramof7@dot.gov', 9158166183, 0, 0, NULL, 0, 0, 0, 0, NULL);
+INSERT INTO `passenger` (`passengerID`, `passportNo`, `nationality`, `firstName`, `middleName`, `lastName`, `DOB`, `gender`, `street`, `city`, `state`, `zipcode`, `email`, `phone`, `inChina`, `date_inChina`, `fever`, `cough`, `difficultyBreathing`, `inRegions`, `recentCountries`) VALUES
+(1, '111111111', 'Chinese', 'Nico', NULL, 'Wang', NULL, 'female', NULL, NULL, NULL, NULL, 'nicowang@gmail.com', 1112223333, 1, '2020-03-02', 0, 0, 0, 0, NULL),
+(2, 'K12342352', 'British', 'Joellyn', NULL, 'Panton', '1954-01-08', 'Female', '7690 Florence Parkway', 'Nashville', 'TN', 37245, 'jpanton0@discovery.com', 6151307355, 0, NULL, 0, 0, 0, 0, NULL),
+(3, 'G35424432', 'American', 'Rutherford', NULL, 'Wisker', '1975-04-05', 'Male', '9333 Tennessee Circle', 'Jacksonville', 'FL', 32220, 'rwisker1@acquirethisname.com', 9047951236, 0, NULL, 0, 0, 0, 0, NULL),
+(4, 'G223187', 'Italian', 'Lucia', 'B', 'Rossi', '1992-02-11', 'female', '100 Boylston st', 'Boston', 'MA', 20116, 'rossil@gmail.com', 376383231, 0, NULL, 0, 0, 0, 0, NULL),
+(5, '015042343', 'American', 'Alfonso', NULL, 'Velez', '1930-12-02', 'Male', '4 Ruskin Street', 'Charlotte', 'NC', 28220, 'avelez2@soundcloud.com', 7047616229, 1, '2020-03-23', 1, 0, 1, 0, NULL),
+(6, 'X02430033', 'American', 'Jase', NULL, 'Halsall', '1971-08-17', 'Male', '5557 Thackeray Crossing', 'Gainesville', 'GA', 30506, 'jhalsall3@xinhuanet.com', 7702996126, 0, NULL, 1, 0, 1, 1, 'Italy'),
+(7, '233336662', 'American', 'Misty', 'K', 'Dangl', '1960-10-14', 'Female', '27 Ohio Avenue', 'Des Moines', 'NY', 50936, 'mdangl4@netlog.com', 5151137228, 1, '2020-01-01', 0, 1, 0, 0, NULL),
+(8, '673232412', 'American', 'Towney', NULL, 'Huyghe', '1937-04-25', 'Male', '40 Vermont Junction', 'Houston', 'TX', 77020, 'thuyghe5@shop-pro.jp', 7136167287, 1, '2020-01-24', 0, 1, 1, 1, 'Finland'),
+(9, '687935325', 'American', 'Benji', NULL, 'Boman', '1930-09-03', 'Male', '2785 Drewry Avenue', 'El Paso', 'TX', 79916, 'bboman6@twitpic.com', 9151320033, 1, '2020-02-29', 0, 0, 0, 0, NULL),
+(10, '337352266', 'American', 'Sidonnie', NULL, 'Abramof', '1933-08-13', 'Female', '5805 Talmadge Alley', 'El Paso', 'MA', 79950, 'sabramof7@dot.gov', 9158166183, 0, NULL, 0, 0, 0, 0, NULL),
+(91343, '1', 'algerian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91344, '231', 'bahamian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91345, '231', 'bahamian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91346, '123', 'algerian', 'Kevin', NULL, 'Huynh', '0000-00-00', 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91347, '2', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91348, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91349, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91350, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91351, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91352, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91353, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91354, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91355, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91356, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL),
+(91357, '1', 'albanian', 'Kevin', NULL, 'Huynh', NULL, 'male', '550 Huntington Ave, MS2248', 'Boston', 'MA', 2115, 'wingler101@gmail.com', 6176204624, 0, NULL, 0, 0, 0, 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -144,7 +166,8 @@ ALTER TABLE `flight`
 -- Indexes for table `layover`
 --
 ALTER TABLE `layover`
-  ADD PRIMARY KEY (`flightID`,`flightNo`);
+  ADD PRIMARY KEY (`layoverID`),
+  ADD KEY `FK2` (`flightID`);
 
 --
 -- Indexes for table `passenger`
@@ -160,13 +183,19 @@ ALTER TABLE `passenger`
 -- AUTO_INCREMENT for table `flight`
 --
 ALTER TABLE `flight`
-  MODIFY `flightID` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98090;
+  MODIFY `flightID` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98107;
+
+--
+-- AUTO_INCREMENT for table `layover`
+--
+ALTER TABLE `layover`
+  MODIFY `layoverID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `passenger`
 --
 ALTER TABLE `passenger`
-  MODIFY `passengerID` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91343;
+  MODIFY `passengerID` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91358;
 
 --
 -- Constraints for dumped tables
@@ -176,7 +205,7 @@ ALTER TABLE `passenger`
 -- Constraints for table `flight`
 --
 ALTER TABLE `flight`
-  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`passengerID`) REFERENCES `passenger` (`passengerID`);
+  ADD CONSTRAINT `FK1` FOREIGN KEY (`passengerID`) REFERENCES `passenger` (`passengerID`);
 
 --
 -- Constraints for table `layover`
